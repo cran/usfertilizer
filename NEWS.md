f2f3dcffc5a59971c36ba740fba0ef3ab036df1f
Wenlong Liu <wliu14@ncsu.edu>
2018-04-02 05:15:07 +0000

    version 0.1.4

---
+++ NEWS.md
@@ -0,0 +1,28 @@
+# usfertilizer 0.1.0
+
+* Decided to change the type of package into a pure data package.
+* Removed irrelevant codes.
+
+# usfertilizer 0.1.1
+
+* Update the description to pass the CRAN comments.
+
+# usfertilizer 0.1.2
+
+* Passed CRAN checks and available at CRAN.
+
+# usfertilizer 0.1.3
+
+* Added a `NEWS.md` file to track changes to the package.
+* Added the data sources and processes codes.
+* Added a license file and code of conduct.
+* Enable website for project using pkgdown.
+
+# usfertilizer 0.1.4
+
+* Added manure data from 1982 to 2012 (every five years).
+* Update the column names: Fertilizer -> Nutrient.
+* Added another column: Input.Type to indicate the nutrient input type.
+* Update readme and related articles.
+* Fixed the failed build of vignettes.
+
--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -10,7 +10,6 @@ knitr::opts_chunk$set(
 
 ## ---- eval= FALSE--------------------------------------------------------
 #  install.packages("usfertilizer")
-#  # not available yet.
 
 ## ---- message=FALSE, warning=FALSE---------------------------------------
 require(usfertilizer)
@@ -21,16 +20,18 @@ data("us_fertilizer_county")
 glimpse(us_fertilizer_county)
 
 ## ------------------------------------------------------------------------
-# plot the top 10 nitrogen application in year 1990.
+# plot the top 10 nitrogen application in year 2008.
 # Reorder to make the plot more cleanner.
 year_plot = 2008
 us_fertilizer_county %>%
-  filter(Fertilizer == "N" & Year == year_plot) %>%
+  filter(Nutrient == "N" & Year == year_plot & Input.Type == "Fertilizer" ) %>%
   top_n(10, Quantity) %>%
   ggplot(aes(x=reorder(paste(County,State, sep = ","), Quantity), Quantity, fill = Quantity))+
   scale_fill_gradient(low = "blue", high = "darkblue")+
   geom_col()+
   ggtitle(paste("Top 10 counties with most fertilizer application in the year of", year_plot)) + 
+  scale_y_continuous(name = "Nitrogen from commecial fertilization (kg)")+
+  scale_x_discrete(name = "Counties")+
   coord_flip()+
   theme_bw()
 
@@ -39,7 +40,7 @@ us_fertilizer_county %>%
 # Reorder to make the plot more cleanner.
 year_plot = 1980
 us_fertilizer_county %>%
-  filter(Fertilizer == "P" & Year == 1980) %>% 
+  filter(Nutrient == "P" & Year == 1980 & Input.Type == "Fertilizer") %>% 
   group_by(State) %>% 
   summarise(p_application = sum(Quantity)) %>% 
   as.data.frame() %>% 
@@ -48,7 +49,7 @@ us_fertilizer_county %>%
   scale_fill_gradient(low = "blue", high = "darkblue")+
   geom_col()+
   ggtitle(paste("Top 10 States with most Phosphrus application in the year of", year_plot)) + 
-  scale_y_continuous(name = "Phosphrus from commecial fertilization (kg)")+
+  scale_y_continuous(name = "Phosphrus from commecial fertilizer (kg)")+
   scale_x_discrete(name = "States")+
   theme_bw()+
   coord_flip()
@@ -60,23 +61,31 @@ states = c("NC","SC")
 
 us_fertilizer_county %>% 
   filter(State %in% states & Year %in% year_plot &
-           Farm.Type == "farm") %>% 
-  group_by(State, Year, Fertilizer) %>% 
-  summarise(Quantity = sum(Quantity)) %>% 
+           Farm.Type == "farm" & Input.Type == "Fertilizer") %>% 
+  group_by(State, Year, Nutrient) %>% 
+  summarise(Quantity = sum(Quantity, na.rm = T)) %>% 
   ggplot(aes(x = as.numeric(Year), y = Quantity, color=State)) +
   geom_point() +
   geom_line()+
   scale_x_continuous(name = "Year")+
   scale_y_continuous(name = "Nutrient input quantity (kg)")+
-  facet_wrap(~Fertilizer, scales = "free", ncol = 2)+
+  facet_wrap(~Nutrient, scales = "free", ncol = 2)+
   ggtitle("Estimated nutrient inputs into arable lands by commercial fertilizer\nfrom 1945 to 2010 in Carolinas")+
   theme_bw()
 
 
 ## ------------------------------------------------------------------------
 us_fertilizer_county %>% 
-  select(Year) %>% 
-   group_by(Year) %>% 
-  filter(row_number() ==1) %>% 
-  arrange(Year)
+  filter(State %in% states & Year %in% year_plot &
+           Farm.Type == "farm" & Nutrient == "N") %>% 
+  group_by(State, Year, Input.Type) %>% 
+  summarise(Quantity = sum(Quantity, na.rm = T)) %>% 
+  ggplot(aes(x = as.numeric(Year), y = Quantity, color=Input.Type)) +
+  geom_point() +
+  geom_line()+
+  scale_x_continuous(name = "Year")+
+  scale_y_continuous(name = "Nutrient input quantity (kg)")+
+  facet_wrap(~State, scales = "free", ncol = 2)+
+  ggtitle("Estimated nitrogen inputs into arable lands by commercial fertilizer and manure\nfrom 1945 to 2012 in Carolinas")+
+  theme_bw()
 
